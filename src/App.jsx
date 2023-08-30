1315750415a5268a6e3a9ebb48597edb30afe75b
Lo_Q <qianqian_luo95@163.com>
2023-08-30 13:08:59 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import {Container, Row, Col } from 'react-bootstrap';
+
+function App() {
+
+  return (
+    <Container>
+      <Row>
+        <Col>events</Col>
+        <Col>tasks</Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default App;
